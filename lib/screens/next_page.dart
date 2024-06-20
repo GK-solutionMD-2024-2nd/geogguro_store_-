71361604558df51fb336d677d9deb033c0bb9b72
Bosslime <dldbstjd2007@naver.com>
2024-06-20 11:23:41 +0900

    수정 . 중2

---
+++ lib/screens/next_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatefulWidget {
+  @override
+  _NextPageState createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      Text("하이")
+    );
+  }
+
+}
